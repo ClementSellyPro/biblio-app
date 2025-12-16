0d0e0813b0efa443375fd17f1017b67e0edf58e6
ClementSellyPro <clementselly.pro@gmail.com>
2025-12-16 14:20:27 +0400

    FIX: add configuration in next.config to accept images from books.google.com

--- app/(main)/feed/_components/PostCard.tsx
+++ app/(main)/feed/_components/PostCard.tsx
@@ -1,7 +1,7 @@
 import { PostWithAuthor } from "@/app/models/PostType";
 import PostCardFooter from "./PostCardFooter";
 import PostCardHeader from "./PostCardHeader";
-// import Image from "next/image";
+import Image from "next/image";
 
 interface PostCardType {
   postData: PostWithAuthor;
@@ -11,16 +11,17 @@ export default function PostCard({ postData }: PostCardType) {
   return (
     <div>
       <PostCardHeader postAuthor={postData.author} />
-      <p className="h-[400px] bg-blue-300 text-transparent">
-        {/* {postData.image && (
+      <div className="flex justify-center items-center h-[550px] overflow-hidden">
+        {postData.image && (
           <Image
             src={postData.image}
             alt={postData.title}
-            width={400}
-            height={400}
+            height={450}
+            width={450}
+            className="object-cover"
           />
-        )} */}
-      </p>
+        )}
+      </div>
       <PostCardFooter
         title={postData.title}
         comment={postData.content}
